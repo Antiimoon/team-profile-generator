98dad6d8c9eaabb2f78e55f91577dcb9e9ac236e
Antoinette Munoz <93494243+Antiimoon@users.noreply.github.com>
2022-01-29 21:22:18 -0800

    html index.js

--- index.js
+++ index.js
@@ -200,3 +200,66 @@ const internGenerator = () => {
 const bottomHtmlFile = () => {
     fs.appendFileSync('index.html', generateBtm());
 };
+
+//engineer card html
+const generateEng = (engineer) => {
+    return `<div class="container mb-3 card-custom shadow-lg border-primary rounded">
+    <div class="row">
+        <div class="team-area col-12 d-flex justify-content-center">
+            <h2 class="engineer">Engineer
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-alien" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                <path d="M11 17a2.5 2.5 0 0 0 2 0" />
+                <path d="M12 3c-4.664 0 -7.396 2.331 -7.862 5.595a11.816 11.816 0 0 0 2 8.592a10.777 10.777 0 0 0 3.199 3.064c1.666 1 3.664 1 5.33 0a10.777 10.777 0 0 0 3.199 -3.064a11.89 11.89 0 0 0 2 -8.592c-.466 -3.265 -3.198 -5.595 -7.862 -5.595z" />
+                <line x1="8" y1="11" x2="10" y2="13" />
+                <line x1="16" y1="11" x2="14" y2="13" />
+            </svg>
+            </h2>     
+        </div>
+    </div>
+            <div class="employee">
+                <p>Name: ${engineer.name}</p>
+                <p>Email: <a href="mailto:${engineer.email}">${engineer.email}</a></p>
+                <p>Id: ${engineer.id}</p>
+                <p>Role: ${engineer.role}</p>
+                <p>Github: <a href="https://github.com/${engineer.github}">${engineer.github}</a></p>
+            </div>
+</div>`
+}
+
+ //manager card html
+const generateMgr = (manager) => {
+    return `<div class="container mb-3 card-custom shadow-lg border-primary rounded">
+    <div class="row">
+        <div class="team-area col-12 d-flex justify-content-center">
+            <h2 class="manager">Manager
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-drone" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                <path d="M10 10h4v4h-4z" />
+                <line x1="10" y1="10" x2="6.5" y2="6.5" />
+                <path d="M9.96 6a3.5 3.5 0 1 0 -3.96 3.96" />
+                <path d="M14 10l3.5 -3.5" />
+                <path d="M18 9.96a3.5 3.5 0 1 0 -3.96 -3.96" />
+                <line x1="14" y1="14" x2="17.5" y2="17.5" />
+                <path d="M14.04 18a3.5 3.5 0 1 0 3.96 -3.96" />
+                <line x1="10" y1="14" x2="6.5" y2="17.5" />
+                <path d="M6 14.04a3.5 3.5 0 1 0 3.96 3.96" />
+            </svg>
+            </h2>
+        </div>
+    </div>
+            <div class="employee">
+                <p>Name: ${manager.name}</p>
+                <p>Email: <a href="mailto:${manager.email}">${manager.email}</a></p>
+                <p>Id: ${manager.id}</p>
+                <p>Role: ${manager.role}</p>
+                <p>Office Number: ${manager.officeNumber}</p>
+        </div>
+</div>`
+}
+
+const generateBtm = () => {
+    return `</body>
+    </html>`
+
+}

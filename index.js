e11cc3621fcc55656bac1cbec284b0c8a5020e64
Antoinette Munoz <93494243+Antiimoon@users.noreply.github.com>
2022-01-29 20:29:16 -0800

    confirm team member/question1

--- index.js
+++ index.js
@@ -8,3 +8,52 @@ const Intern = require('./lib/intern');
 const managers = [];
 const engineers = [];
 const interns = [];
+
+const compileTeamMember = () => {
+    inquirer
+    .prompt ([
+        {
+            type: 'confirm',
+            name: 'compileTeamMember',
+            message: 'Would you like to add another team member?',
+        },
+    ])
+    .then((answers) => {
+        if(answers.compileTeamMember === true) {
+            question1();
+        } else {
+            console.log(managers, engineers, interns);
+            module.exports = managers;
+            module.exports = engineers;
+            module.exports = interns;
+            deleteHtml();
+            managerGenerator();
+            engineerGenerator();
+            internGenerator();
+            return answers;
+        }
+    });
+};
+
+const question1 = () => {
+    inquirer
+    .prompt ([
+        {
+            type: 'list',
+            name: 'role',
+            message: 'What is employees role?',
+            choices: ['Manager', 'Engineer', 'Intern'],
+        },
+    ])
+    .then((answers) => {
+        if (answers.role === 'Manager') {
+            managerQuestions();
+        } else if (answers.role === 'Engineer') {
+            engineerQuestions();
+        } else if (answers.role === 'Intern') {
+            internQuestions();
+        }
+    });
+};
+
+question1();
